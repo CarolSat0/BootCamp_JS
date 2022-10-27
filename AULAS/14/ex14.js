109d178bfbd7ede54387a993a0fa4686d925cc29
Carol <carolsato99@gmail.com>
2022-10-27 17:03:44 -0300

    Aulas 14, 15 e 16

--- AULAS/14/ex14.js
+++ AULAS/14/ex14.js
@@ -1,24 +1,47 @@
-let frutas = [
-    {fruta: "Maça", preco: 3.5},
-    {fruta: "Melancia", preco: 5},
-]
+// let frutas = [
+//     {fruta: "Maça", preco: 3.5},
+//     {fruta: "Melancia", preco: 5},
+// ]
 
-function listaFrutas(f){
-    for(let produto of f){ //produto percorre frutas
-        for(let p in produto){
-            console.log(`${p} -> ${produto[p]}`)
+// function listaFrutas(f){
+//     for(let produto of f){ //produto percorre frutas
+//         for(let p in produto){
+//             console.log(`${p} -> ${produto[p]}`)
+//         }
+//     }
+// }
+
+// listaFrutas(frutas);
+
+// function SomaValorFrutas(vf){
+//     for(let produto of vf){
+//         for(let f in produto){
+//             if(typeof)
+//         }
+//     }
+// }
+
+// SomaValorFrutas(frutas);
+
+window.onload=function(){
+    function tabuada(numero){
+        let tabuada = 0;
+        if(typeof numero !== 'number'){
+            alert("Digire um valor numerico valido");
+        }else{
+            if(numero <1 || numero >10){
+                alert("Digite um valor entre 1 e 10");
+            }else{
+                console.log(`Tabuada do ${numero}`);
+                while(tabuada<=10){ //tabuada é o contador
+                    console.log(`${numero} x ${tabuada} = ${(numero *tabuada)}`);
+                    tabuada++;
+                }
+            }
         }
+
+        
     }
+
+    tabuada(' ');
 }
-
-listaFrutas(frutas);
-
-function SomaValorFrutas(vf){
-    for(let produto of vf){
-        for(let f in produto){
-            console.log(produto[f]+produto[f+1]);
-        }
-    }
-}
-
-SomaValorFrutas(frutas);
---
+++ AULAS/16/ex15.js
@@ -0,0 +1,29 @@
+window.onload=function(){
+    // let numero; 
+    //     function tabuada(numero){
+    //         let tabuada = 0; // contador
+    //         // const botao = document.getElementById(botao);
+    //         botao.addEventListener('click', function(){
+    //         numero = document.getElementById(entrada);
+    //             if(typeof numero !== 'number'){
+    //                 console.log("1 if");
+    //                 alert("Digite numero de 1 a 10");
+    //             }else{
+    //                 if(numero>1 || numero <10){
+    //                     alert("Digite um numero entre 1 a 10");
+    //                 }else{
+    //                     while(tabuada<=10){
+    //                         let resultado = numero*tabuada;
+    //                         console.log(resultado);
+    //                         tabuada++;
+    //                     }
+    //                 }
+    //             }
+    //     })
+    // }
+    // tabuada(document.getElementById(entrada));
+
+
+    const tab = document.querySelector("#tabuada");
+    const btn = document.qua
+}
