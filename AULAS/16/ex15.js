5d03466cf58b20733f12d179d72907b94a5ead7f
Carol <carolsato99@gmail.com>
2022-10-28 11:46:42 -0300

    completado as aulas 15 e 16

--- AULAS/16/ex15.js
+++ AULAS/16/ex15.js
@@ -1,29 +1,30 @@
 window.onload=function(){
-    // let numero; 
     //     function tabuada(numero){
-    //         let tabuada = 0; // contador
-    //         // const botao = document.getElementById(botao);
-    //         botao.addEventListener('click', function(){
-    //         numero = document.getElementById(entrada);
-    //             if(typeof numero !== 'number'){
-    //                 console.log("1 if");
-    //                 alert("Digite numero de 1 a 10");
-    //             }else{
-    //                 if(numero>1 || numero <10){
-    //                     alert("Digite um numero entre 1 a 10");
-    //                 }else{
-    //                     while(tabuada<=10){
-    //                         let resultado = numero*tabuada;
-    //                         console.log(resultado);
-    //                         tabuada++;
-    //                     }
-    //                 }
-    //             }
-    //     })
-    // }
-    // tabuada(document.getElementById(entrada));
-
+    //         
+    
+            
+    //     }
 
     const tab = document.querySelector("#tabuada");
-    const btn = document.qua
+    const btn = document.querySelector("#Calcular");
+    const resp = document.querySelector("#resposta");
+    
+    btn.addEventListener('click', function(){
+        let numero = Number(tab.value);
+        let tabuada = 0;
+            if(isNaN(numero)){
+                resp.innerHTML = "Digire um valor numerico valido";
+            }else{
+                if(numero <1 || numero >10){
+                    resp.innerHTML = "Digite um valor entre 1 e 10";
+                }else{
+                    console.log(`Tabuada do ${numero}`);
+                    resp.innerHTML = `tabuada do ${tab.value}`
+                    while(tabuada<=10){ //tabuada é o contador
+                        resp.innerHTML +=`<br> ${numero} x ${tabuada} = ${(numero * tabuada)}`;
+                        tabuada++;
+                    }
+                }
+            }
+    });
 }
